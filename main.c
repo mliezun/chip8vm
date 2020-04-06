9eeb8169f503be270bdce238e25b422567d6888f
Miguel Liezun <liezun.js@gmail.com>
2020-04-06 20:39:09 -0300

    Mutexes added

--- main.c
+++ main.c
@@ -24,18 +24,6 @@ uint16_t pc;
 // I register
 uint16_t I;
 
-// Timers -> lookup more informantion
-// There is a delay timer and a sound timer
-
-// Input -> Hex keyboard, 16 keys (0x0 to 0xF)
-/*
-Layout
-1	2	3	C
-4	5	6	D
-7	8	9	E
-A	0	B	F
-*/
-
 // Screen 64x32 pixels monochrome (1bit each pixel)
 uint32_t screen[64];
 
@@ -67,9 +55,24 @@ void destroy_sdl();
 void cls_sdl();
 
 /* */
-pthread_t emulator_thread;
+pthread_t emulator_thread, timers_thread;
 int init_emulator();
+int init_timers();
 void *emulator_loop(void *arg);
+void *timers_loop(void *arg);
+pthread_mutex_t emulator_mutex;
+uint8_t delay_timer;
+uint8_t sound_timer;
+
+// Hex keyboard, 16 keys (0x0 to 0xF)
+uint16_t keys_pressed;
+/*
+Layout
+1	2	3	C
+4	5	6	D
+7	8	9	E
+A	0	B	F
+*/
 
 int main(void)
 {
@@ -95,6 +98,9 @@ int main(void)
 
     /* Destroy sdl objects */
     destroy_sdl(&sdl);
+
+    /* Destroy mutex */
+    pthread_mutex_destroy(&emulator_mutex);
 
     return 0;
 }
@@ -130,14 +136,132 @@ int init_sdl()
     return 0;
 }
 
+static inline void set_key(SDL_Keycode k)
+{
+    pthread_mutex_lock(&emulator_mutex);
+    switch (k) {
+        case SDLK_0:
+            keys_pressed |= 1;
+            break;
+        case SDLK_1:
+            keys_pressed |= 1 << 1;
+            break;
+        case SDLK_2:
+            keys_pressed |= 1 << 2;
+            break;
+        case SDLK_3:
+            keys_pressed |= 1 << 3;
+            break;
+        case SDLK_4:
+            keys_pressed |= 1 << 4;
+            break;
+        case SDLK_5:
+            keys_pressed |= 1 << 5;
+            break;
+        case SDLK_6:
+            keys_pressed |= 1 << 6;
+            break;
+        case SDLK_7:
+            keys_pressed |= 1 << 7;
+            break;
+        case SDLK_8:
+            keys_pressed |= 1 << 8;
+            break;
+        case SDLK_9:
+            keys_pressed |= 1 << 9;
+            break;
+        case SDLK_a:
+            keys_pressed |= 1 << 10;
+            break;
+        case SDLK_b:
+            keys_pressed |= 1 << 10;
+            break;
+        case SDLK_c:
+            keys_pressed |= 1 << 10;
+            break;
+        case SDLK_d:
+            keys_pressed |= 1 << 10;
+            break;
+        case SDLK_e:
+            keys_pressed |= 1 << 10;
+            break;
+        case SDLK_f:
+            keys_pressed |= 1 << 10;
+            break;
+    }
+    pthread_mutex_unlock(&emulator_mutex);
+}
+
+static inline void clear_key(SDL_Keycode k)
+{
+    pthread_mutex_lock(&emulator_mutex);
+    switch (k) {
+        case SDLK_0:
+            keys_pressed &= 0xFFFE;
+            break;
+        case SDLK_1:
+            keys_pressed &= 0xFFFD;
+            break;
+        case SDLK_2:
+            keys_pressed &= 0xFFFB;
+            break;
+        case SDLK_3:
+            keys_pressed &= 0xFFF7;
+            break;
+        case SDLK_4:
+            keys_pressed &= 0xFFEF;
+            break;
+        case SDLK_5:
+            keys_pressed &= 0xFFDF;
+            break;
+        case SDLK_6:
+            keys_pressed &= 0xFFBF;
+            break;
+        case SDLK_7:
+            keys_pressed &= 0xFF7F;
+            break;
+        case SDLK_8:
+            keys_pressed &= 0xFEFF;
+            break;
+        case SDLK_9:
+            keys_pressed &= 0xFDFF;
+            break;
+        case SDLK_a:
+            keys_pressed &= 0xFBFF;
+            break;
+        case SDLK_b:
+            keys_pressed &= 0xF7FF;
+            break;
+        case SDLK_c:
+            keys_pressed &= 0xEFFF;
+            break;
+        case SDLK_d:
+            keys_pressed &= 0xDFFF;
+            break;
+        case SDLK_e:
+            keys_pressed &= 0xBFFF;
+            break;
+        case SDLK_f:
+            keys_pressed &= 0x7FFF;
+            break;
+    }
+    pthread_mutex_unlock(&emulator_mutex);
+}
+
 void event_loop_sdl()
 {
     SDL_Event e;
     bool quit = false;
     while (!quit){
         while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT){
+            if (e.type == SDL_QUIT) {
                 quit = true;
+            }
+            if (e.type == SDL_KEYDOWN) {
+                set_key(e.key.keysym.sym);
+            }
+            if (e.type == SDL_KEYUP) {
+                clear_key(e.key.keysym.sym);
             }
         }
     }
@@ -166,12 +290,42 @@ int init_emulator()
 {
     /*creates a new thread with default attributes and NULL passed as the argument to the start routine*/
     int err;
-    if ((err = pthread_create(&emulator_thread, NULL, emulator_loop, NULL))) /*check whether the thread creation was successful*/
-    {
+    /*check whether the thread creation was successful*/
+    if ((err = pthread_create(&emulator_thread, NULL, emulator_loop, NULL))) {
         printf("pthread_create error creating emulator thread\n");
         return err;
     }
+    if ((err = init_timers())) {
+        return err;
+    }
+    pthread_mutex_init(&emulator_mutex, NULL);
     return 0;
+}
+
+int init_timers()
+{
+    int err;
+    if ((err = pthread_create(&timers_thread, NULL, timers_loop, NULL))) {
+        printf("pthread_create error creating emulator timers thread\n");
+        return err;
+    }
+    delay_timer = 0;
+    sound_timer = 0;
+    return 0;
+}
+
+void *timers_loop(void *arg)
+{
+    while (1) {
+        pthread_mutex_lock(&emulator_mutex);
+        if (delay_timer > 0) delay_timer--;
+        if (sound_timer > 0) {
+            if (sound_timer == 1) SDL_PauseAudio(1);
+            sound_timer--;
+        }
+        pthread_mutex_unlock(&emulator_mutex);
+        usleep(16667);
+    }
 }
 
 void *emulator_loop(void *arg)
@@ -298,8 +452,68 @@ void decode_execute()
         case 0xD:
             break;
         case 0xE:
+            pthread_mutex_lock(&emulator_mutex);
+            if (((1 << vx) | keys_pressed)) {
+                pc += 2;
+            }
+            pthread_mutex_unlock(&emulator_mutex);
             break;
         case 0xF:
+            switch (opcode & 0xF) {
+                case 0x07:
+                    pthread_mutex_lock(&emulator_mutex);
+                    vx = delay_timer;
+                    pthread_mutex_unlock(&emulator_mutex);
+                    break;
+                case 0x0A:
+                    while (1) {
+                        pthread_mutex_lock(&emulator_mutex);
+                        if (keys_pressed) {
+                            int8_t pressed = -1;
+                            uint16_t aux = keys_pressed;
+                            while (aux > 0) {
+                                aux >>= 1;
+                                pressed++;
+                            }
+                            vx = pressed;
+                            break;
+                        }
+                        pthread_mutex_unlock(&emulator_mutex);
+                    }
+                    break;
+                case 0x15:
+                    pthread_mutex_lock(&emulator_mutex);
+                    delay_timer = vx;
+                    pthread_mutex_unlock(&emulator_mutex);
+                    break;
+                case 0x18:
+                    pthread_mutex_lock(&emulator_mutex);
+                    sound_timer = vx;
+                    pthread_mutex_unlock(&emulator_mutex);
+                    break;
+                case 0x1E:
+                    I = I + vx;
+                    break;
+                case 0x29:
+                    break;
+                case 0x33:
+                    break;
+                case 0x55:
+                    int i;
+                    for (i = 0; i < ((opcode >> 8) & 0x0F); i++) {
+                        mem[I+i] = registers[i];
+                    }
+                    break;
+                case 0x65:
+                    int i;
+                    for (i = 0; i < ((opcode >> 8) & 0x0F); i++) {
+                        registers[i] = mem[I+i];
+                    }
+                    break;
+                default:
+                    // TODO: handle error
+                    break;
+            }
             break;
     }
 }
